f6386540c878810b1f58d8dd78fd518142c38536
hrefnanamfa <34063060+hrefnanamfa@users.noreply.github.com>
2018-10-11 16:18:24 +0000

    Add implemented for multiple numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,18 @@
 //calculator.js
-function add(number){
-	if(number == "")
+function add(numbers){
+	if(numbers == "")
 	return 0;
 	
-	if(number.includes(",")){
-		var numberArray = number.split(",");
-		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+	if(numbers.includes(",")){
+		var numberArray = numbers.split(",");
+		var total = 0;
+		for(var i = 0; i < numberArray.length; i++){
+			total += parseInt(numberArray[i]);
+		}
+		return total;
 	}
 	else
-		return parseInt(number);
+		return parseInt(numbers);
 }
 
 module.exports = add;
