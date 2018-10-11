3f1c7a9850a430920e95d10a6530cc2e8362450b
hrefnanamfa <34063060+hrefnanamfa@users.noreply.github.com>
2018-10-11 16:25:37 +0000

    Refactoring. Made a function sum to  sumerize multiple numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,18 +1,23 @@
 //calculator.js
 function add(numbers){
 	if(numbers == "")
-	return 0;
+		return 0;
 	
 	if(numbers.includes(",")){
 		var numberArray = numbers.split(",");
-		var total = 0;
-		for(var i = 0; i < numberArray.length; i++){
-			total += parseInt(numberArray[i]);
-		}
-		return total;
+		return sum(numberArray);
 	}
 	else
 		return parseInt(numbers);
+}
+
+
+function sum(numberArray){
+	var total = 0;
+	for(var i = 0; i < numberArray.length; i++){
+		total += parseInt(numberArray[i]);
+	}
+	return total;
 }
 
 module.exports = add;
