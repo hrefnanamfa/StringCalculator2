6be884c6ed3da6f0d6faa313b717ae0b791939fa
hrefnanamfa <34063060+hrefnanamfa@users.noreply.github.com>
2018-10-11 22:59:40 +0000

    Refactoring. Added more tests and more comments on code

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,5 @@
 //calculator.js
 function add(numbers){
-
 	var numberArray;
 
 	if(numbers == "") // if the string is empty
@@ -15,25 +14,26 @@ function add(numbers){
 		numberArray = substr.split(splitter); // make array on delimiter
 	}
 
-	else if(numbers.includes(",") || numbers.includes("\n")){ // if delimiter is , or \n
+	else if(numbers.includes(",") || numbers.includes("\n")) // if delimiter is , or \n
 		numberArray = numbers.split(/[\n,]+/); // make array on , and \n
-	}
+	
 	if(numbers.includes("-")) // if we have negative numbers
 		checkNegativeNumbers(numberArray); // throw error
+
 	return sum(numberArray);
 }
 
-function sum(numberArray){
+function sum(numberArray){ // returns the sum of numbers in string 
 	var total = 0;
 	for(var i = 0; i < numberArray.length; i++){
-		if(parseInt(numberArray[i]) <= 1000){
+		if(parseInt(numberArray[i]) <= 1000){ // if a number is >1000 we ignore it
 			total += parseInt(numberArray[i]);
 		}
 	}
 	return total;
 }
 
-function checkNegativeNumbers(numberArray){
+function checkNegativeNumbers(numberArray){ // throws error for negative numbers in array
 	var negativeArray = []; // create empty negativeArray
 	for(var i = 0; i < numberArray.length; i++){
 		if(parseInt(numberArray[i]) < 0) // if the value in numberArray is < 0
@@ -43,12 +43,12 @@ function checkNegativeNumbers(numberArray){
 		throw new Error("Negatives not allowed: " + negativeArray.toString()); //throw error
 }
 
-function getSplitter(numbers){
-	var i = 2;
-	var splitter = "";
+function getSplitter(numbers){ // get delimiter of string
+	var i = 2; // we know it starts at index 2
+	var splitter = ""; //splitter initialized as empty string
 
-	while(numbers[i] != "\n"){
-		splitter = splitter + numbers[i];
+	while(numbers[i] != "\n"){ // and it ends on index where numbers[i] == \n
+		splitter = splitter + numbers[i]; // delimiter could be more than one character
 		i++;
 	}
 	return splitter;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -17,8 +17,23 @@ it("should sumerize multiple numbers", () => {
 	expect(add("1,2,3")).toBe(6);
 });
 
+it("should sumerize multiple numbers", () => {
+	expect(add("4,10,6")).toBe(20);
+});
+
 it("should sumerize multiple numbers on , and new lines", () => {
 	expect(add("1\n2,3")).toBe(6);
+});
+
+it("should sumerize multiple numbers on , and new lines", () => {
+	expect(add("1\n4\n5")).toBe(10);
+});
+
+it("should return exception 'Negatives not allowed:' and list all negative numbers", () => {
+	function throwError() {
+		add("-1,2");
+	}
+	expect(throwError).toThrowError("Negatives not allowed: -1");
 });
 
 it("should return exception 'Negatives not allowed:' and list all negative numbers", () => {
@@ -39,6 +54,22 @@ it("should ignore numbers bigger than 1000", () => {
 	expect(add("1001,2")).toBe(2);
 });
 
+it("should ignore numbers bigger than 1000", () => {
+	expect(add("1000,2")).toBe(1002);
+});
+
+it("should ignore numbers bigger than 1000", () => {
+	expect(add("100000\n2")).toBe(2);
+});
+
+it("should ignore numbers bigger than 1000", () => {
+	expect(add("999,2")).toBe(1001);
+});
+
 it("should split numbers with given delimiter", () => {
 	expect(add("//;\n1;2")).toBe(3);
+});
+
+it("should split numbers with given delimiter", () => {
+	expect(add("//!\n1!2!3!4")).toBe(10);
 });
